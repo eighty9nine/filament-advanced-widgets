2fd8674dd03d47a9a43cebe1574df4df5070d496
Tom Janssen <tom@dcc.team>
2025-02-06 14:17:56 +0100

    extend filament's class

--- src/AdvancedWidgetConfiguration.php
+++ src/AdvancedWidgetConfiguration.php
@@ -1,23 +1,10 @@
 <?php
 
-namespace  EightyNine\FilamentAdvancedWidget;
+namespace EightyNine\FilamentAdvancedWidget;
 
-class AdvancedWidgetConfiguration
+use Filament\Widgets\WidgetConfiguration;
+
+class AdvancedWidgetConfiguration extends WidgetConfiguration
 {
-    /**
-     * @param  class-string<Widget>  $widget
-     * @param  array<string, mixed>  $properties
-     */
-    public function __construct(
-        readonly public string $widget,
-        protected array $properties = [],
-    ) {}
-
-    /**
-     * @return array<string, mixed>
-     */
-    public function getProperties(): array
-    {
-        return $this->properties;
-    }
+    //
 }
