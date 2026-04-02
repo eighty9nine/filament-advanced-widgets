7e0845a96b521702daeb72ec507364e076d5a391
WebplusM <contact@webplusm.net>
2026-04-02 16:26:39 -0400

    refactor(ui): replace Filament grid components with custom div-based grid implementation

--- resources/views/components/advanced-widget.blade.php
+++ resources/views/components/advanced-widget.blade.php
@@ -14,22 +14,23 @@
             'default' => $columnStart,
         ];
     }
+
+    $spanClasses = 'fi-wi-widget';
+    if ($columnSpan['default'] ?? null) $spanClasses .= ' col-span-' . $columnSpan['default'];
+    if ($columnSpan['sm'] ?? null) $spanClasses .= ' sm:col-span-' . $columnSpan['sm'];
+    if ($columnSpan['md'] ?? null) $spanClasses .= ' md:col-span-' . $columnSpan['md'];
+    if ($columnSpan['lg'] ?? null) $spanClasses .= ' lg:col-span-' . $columnSpan['lg'];
+    if ($columnSpan['xl'] ?? null) $spanClasses .= ' xl:col-span-' . $columnSpan['xl'];
+    if ($columnSpan['2xl'] ?? null) $spanClasses .= ' 2xl:col-span-' . $columnSpan['2xl'];
+
+    if ($columnStart['default'] ?? null) $spanClasses .= ' col-start-' . $columnStart['default'];
+    if ($columnStart['sm'] ?? null) $spanClasses .= ' sm:col-start-' . $columnStart['sm'];
+    if ($columnStart['md'] ?? null) $spanClasses .= ' md:col-start-' . $columnStart['md'];
+    if ($columnStart['lg'] ?? null) $spanClasses .= ' lg:col-start-' . $columnStart['lg'];
+    if ($columnStart['xl'] ?? null) $spanClasses .= ' xl:col-start-' . $columnStart['xl'];
+    if ($columnStart['2xl'] ?? null) $spanClasses .= ' 2xl:col-start-' . $columnStart['2xl'];
 @endphp
 
-<x-filament::grid.column
-    :default="$columnSpan['default'] ?? 1"
-    :sm="$columnSpan['sm'] ?? null"
-    :md="$columnSpan['md'] ?? null"
-    :lg="$columnSpan['lg'] ?? null"
-    :xl="$columnSpan['xl'] ?? null"
-    :twoXl="$columnSpan['2xl'] ?? null"
-    :defaultStart="$columnStart['default'] ?? null"
-    :smStart="$columnStart['sm'] ?? null"
-    :mdStart="$columnStart['md'] ?? null"
-    :lgStart="$columnStart['lg'] ?? null"
-    :xlStart="$columnStart['xl'] ?? null"
-    :twoXlStart="$columnStart['2xl'] ?? null"
-    :attributes="\Filament\Support\prepare_inherited_attributes($attributes)->class('fi-wi-widget')"
->
+<div {{ $attributes->class($spanClasses) }}>
     {{ $slot }}
-</x-filament::grid.column>
+</div>
--- resources/views/components/advanced-widgets.blade.php
+++ resources/views/components/advanced-widgets.blade.php
@@ -1,20 +1,23 @@
 @props([
-    'columns' => [
-        'lg' => 2,
-    ],
-    'data' => [],
-    'widgets' => [],
-])
+      'columns' => [
+          'lg' => 2,
+      ],
+      'data' => [],
+      'widgets' => [],
+  ])
 
-<x-filament::grid
-    :default="$columns['default'] ?? 1"
-    :sm="$columns['sm'] ?? null"
-    :md="$columns['md'] ?? null"
-    :lg="$columns['lg'] ?? ($columns ? (is_array($columns) ? null : $columns) : 2)"
-    :xl="$columns['xl'] ?? null"
-    :two-xl="$columns['2xl'] ?? null"
-    :attributes="\Filament\Support\prepare_inherited_attributes($attributes)->class('fi-wi gap-6')"
->
+@php
+    $cols = is_array($columns) ? $columns : ['default' => $columns];
+    $gridClasses = 'fi-wi grid gap-6';
+    $gridClasses .= ' grid-cols-' . ($cols['default'] ?? 1);
+    if ($cols['sm'] ?? null) $gridClasses .= ' sm:grid-cols-' . $cols['sm'];
+    if ($cols['md'] ?? null) $gridClasses .= ' md:grid-cols-' . $cols['md'];
+    if ($cols['lg'] ?? null) $gridClasses .= ' lg:grid-cols-' . ($cols['lg'] ?? 2);
+    if ($cols['xl'] ?? null) $gridClasses .= ' xl:grid-cols-' . $cols['xl'];
+    if ($cols['2xl'] ?? null) $gridClasses .= ' 2xl:grid-cols-' . $cols['2xl'];
+@endphp
+
+<div {{ $attributes->class($gridClasses) }}>
     @php
         $normalizeWidgetClass = function (string | Filament\Widgets\WidgetConfiguration $widget): string {
             if ($widget instanceof \Filament\Widgets\WidgetConfiguration) {
@@ -32,8 +35,9 @@
 
         @livewire(
             $widgetClass,
-            [...(($widget instanceof \Filament\Widgets\WidgetConfiguration) ? [...$widget->widget::getDefaultProperties(), ...$widget->getProperties()] : $widget::getDefaultProperties()), ...$data],
+            [...(($widget instanceof \Filament\Widgets\WidgetConfiguration) ? [...$widget->widget::getDefaultProperties(), ...$widget->getProperties()] : $widget::getDefaultProperties()),
+...$data],
             key("{$widgetClass}-{$widgetKey}"),
         )
     @endforeach
-</x-filament::grid>
+</div>
